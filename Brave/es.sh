4cca0a9431c8e2fec0b0c97f5f8fb7f2287bd62b
XJH-gh <xiaojiehuang011@gmail.com>
2025-09-12 21:02:29 +0200

    Update es.sh

--- Brave/es.sh
+++ Brave/es.sh
@@ -47,7 +47,7 @@ echo " - Más ligero que Chrome, consume menos memoria"
 echo " - Menos procesos en segundo plano, interfaz más limpia"
 echo
 echo "⚠️ Inconvenientes:"
-echo " - NECESITAS DAR MINIMO UN 3GB"
+echo "- Necesitas mínimo 3 GB de espacio libre para linux"
 echo " - Algunas webs/extensiones pueden ir lentas"
 echo " - Configuración de privacidad estricta, hay que ajustar permisos manualmente"
 echo " - En CrazyGames puede ir lento, o otros mas."
